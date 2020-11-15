6c111344601afbd6b750f943a47f228406e63bb1
Irina <irensaywhen@gmail.com>
2020-11-15 11:32:08 +0300

    Layout moved to containers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Layout from './components/Layout/Layout';
+import Layout from './containers/Layout/Layout';
 import BurderBuilder from './containers/BurgerBuilder/BurderBuilder';
 
 export default class App extends Component {
