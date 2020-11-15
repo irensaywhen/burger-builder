f7dbb0ad79aea771e67cd6e866a72bbc949df9f1
Irina <irensaywhen@gmail.com>
2020-11-15 11:25:16 +0300

    Improving perfomance

--- src/components/Navigation/HamburgerIcon/HamburgerIcon.js
+++ src/components/Navigation/HamburgerIcon/HamburgerIcon.js
@@ -4,7 +4,7 @@ import classes from './HamburgerIcon.module.css';
 const HamburgerIcon = ({ clickHandler }) => {
   return (
     <div className={classes.HamburgerIcon} onClick={clickHandler}>
-      <i class='fas fa-bars'></i>
+      <i className='fas fa-bars'></i>
     </div>
   );
 };
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,27 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import Backdrop from '../Backdrop/Backdrop';
 import classes from './Modal.module.css';
 
-const Modal = ({ show, children, backdropClickHandler }) => {
-  return (
-    <Fragment>
-      <Backdrop show={show} clickHandler={backdropClickHandler} />
-      <div
-        style={{
-          transform: show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: show ? '1' : '0',
-        }}
-        className={classes.Modal}
-      >
-        {children}
-      </div>
-    </Fragment>
-  );
-};
+export default class Modal extends Component {
+  shouldComponentUpdate(nextProps) {
+    return nextProps.show !== this.props.show;
+  }
 
-export default Modal;
+  render() {
+    const { show, children, backdropClickHandler } = this.props;
+    return (
+      <Fragment>
+        <Backdrop show={show} clickHandler={backdropClickHandler} />
+        <div
+          style={{
+            transform: show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: show ? '1' : '0',
+          }}
+          className={classes.Modal}
+        >
+          {children}
+        </div>
+      </Fragment>
+    );
+  }
+}
